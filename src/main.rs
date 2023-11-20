f7c8aa8485b64d156986eb83f91c2434b05c63f7
Sax Dhiut <mail@iyxeyl.me>
2023-11-20 19:11:26 +0400

    refactor: change git tracking functions in main.rs
    Improved error handling in functions that interact with a git repository in main.rs. Functions will now return a Result to handle potential errors more gracefully, removing unnecessary use of expects in the process. Paths of modified git files are now also properly displayed.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use git2::{Delta, DiffDelta, DiffOptions, Repository, Status, StatusOptions};
 use std::env;
-use std::path::{Display, Path, PathBuf};
+use std::path::{Path, PathBuf};
+
+use git2::{Delta, DiffOptions, Repository};
 
 fn find_git_directory(start_path: &Path) -> Option<PathBuf> {
     let mut current_path = start_path.to_path_buf();
@@ -17,23 +18,27 @@ fn find_git_directory(start_path: &Path) -> Option<PathBuf> {
     None
 }
 
-fn get_repo() -> Repository {
-    let current_path = env::current_dir().expect("Error getting current directory");
-    let repo_directory = find_git_directory(&current_path).expect("Unable to find .git directory");
-    Repository::open(repo_directory).expect("Error opening repository")
+fn get_repo() -> Result<Repository, &'static str> {
+    let current_path = env::current_dir().map_err(|_| "Unable to get current directory")?;
+
+    let repo_directory =
+        find_git_directory(&current_path).ok_or("Unable to find .git directory")?;
+
+    Repository::open(repo_directory).map_err(|_| "Error opening repository")
 }
+
 #[derive(Debug)]
 struct File {
     status: Delta,
     path: PathBuf,
 }
 
-fn get_modified_files(repo: &Repository) -> Vec<File> {
+fn get_modified_files(repo: &Repository) -> Result<Vec<File>, &'static str> {
     let mut modified_files = Vec::new();
 
     let diffs = repo
         .diff_index_to_workdir(None, Some(DiffOptions::new().include_untracked(true)))
-        .expect("Error getting diffs");
+        .map_err(|_| "Error getting diff")?;
 
     diffs
         .foreach(
@@ -51,15 +56,29 @@ fn get_modified_files(repo: &Repository) -> Vec<File> {
             None,
             None,
         )
-        .expect("Error iterating over diffs");
+        .map_err(|_| "Error iterating over diff")?;
 
-    modified_files
+    Ok(modified_files)
 }
 
 fn main() {
-    let repo = get_repo();
+    let repo = match get_repo() {
+        Ok(repo) => repo,
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            return;
+        }
+    };
 
-    let modified_files = get_modified_files(&repo);
+    let modified_files = match get_modified_files(&repo) {
+        Ok(files) => files,
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            return;
+        }
+    };
 
-    modified_files.iter().for_each(|f| println!("{:?}", f))
+    modified_files
+        .iter()
+        .for_each(|f| println!("{:?}: {}", f.status, f.path.display()))
 }
