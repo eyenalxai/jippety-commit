0b1758fde94a693ca7eb298efba36b2f721557d0
Sax Dhiut <mail@iyxeyl.me>
2023-11-23 00:24:49 +0400

    feat: Add configuration parsing and API key validation
    Added the ability to parse app configuration from a TOML file, which includes retrieval of the OpenAI API key. Implemented API key validation to ensure that the provided key starts with "sk-" and is of a sufficient length. Enhanced main function to use the fetched API key or report an error.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,39 @@
 use std::error::Error;
+use std::fs::read_to_string;
 use std::process::Command;
+
+use dirs::home_dir;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    openai_api_key: String,
+}
+
+const CONFIG_LOCATION: &str = ".config/jippety-kohmmit/config.toml";
+
+fn get_config() -> Result<Config, Box<dyn Error>> {
+    let home_folder = home_dir().ok_or("Home directory not found")?;
+    let config_path = home_folder.join(CONFIG_LOCATION);
+    let contents =
+        read_to_string(config_path).map_err(|e| format!("Failed to read config\n{}", e))?;
+    Ok(toml::from_str(&contents)?)
+}
+
+fn get_api_key() -> Result<String, Box<dyn Error>> {
+    let config = get_config()?;
+    let api_key = config.openai_api_key;
+
+    if !api_key.starts_with("sk-") {
+        return Err("OpenAI API key must start with sk-".into());
+    }
+
+    if api_key.len() < 32 {
+        return Err("OpenAI API key is too short".into());
+    }
+
+    Ok(api_key)
+}
 
 fn get_diff() -> Result<String, Box<dyn Error>> {
     let output = Command::new("git")
@@ -14,8 +48,13 @@ fn get_diff() -> Result<String, Box<dyn Error>> {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let diff = get_diff()?;
-
-    println!("{}", diff);
+    let api_key = match get_api_key() {
+        Ok(api_key) => api_key,
+        Err(e) => {
+            println!("Error: {}", e);
+            return Ok(());
+        }
+    };
+    println!("{:?}", api_key);
     Ok(())
 }
